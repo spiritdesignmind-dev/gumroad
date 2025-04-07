cc2d1ddc65e099ef4694612359cec27b117c19f7
Ershad Kunnakkadan <ershad92@gmail.com>
2025-04-07 17:22:03 +0400

    Update Action Cable URL configuration for production and staging environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
+  config.action_cable.url = ENV.fetch("ANYCABLE_WEBSOCKET_URL", "wss://#{ANYCABLE_HOST}/cable")
   config.action_cable.allowed_request_origins = ["#{PROTOCOL}://#{DOMAIN}"]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
