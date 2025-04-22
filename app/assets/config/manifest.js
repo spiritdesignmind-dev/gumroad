885c8003a23f7388c22dfa98e9bcf3cc2b64fdbe
sumancloudchef <textsumanb@gmail.com>
2025-04-22 10:34:02 +0530

    feat: why-gumroad page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_tree ../fonts
 
 //= link_directory ../javascripts .js
+
+//= link style.css
--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class HelpController < Sellers::BaseController
-    skip_after_action :verify_authorized
+  skip_after_action :verify_authorized
 
-    def index
+  def index
+    @title = "Help"
+    @on_help_page = true
+    @body_class = "help-container"
+  end
 
-      @title = "Help Center"
-      @on_help_page = true
-      @title = "Help"
-      @body_class = "help-container"
-    end
-
+  def why_gumroad
+    @title = "Why Gumroad"
+    @on_help_page = true
+  end
 end
