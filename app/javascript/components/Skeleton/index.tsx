4bda8d89fa4a6f53a7e0183552e00fec0cedf6f9
Pradumn <pradumn@tealfeed.com>
2025-08-28 05:42:59 +0530

    fix: classnames variable name

--- app/javascript/components/Skeleton/index.tsx
+++ app/javascript/components/Skeleton/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { classnames } from "$app/utils/classnames";
+import { classNames } from "$app/utils/classNames";
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div data-slot="skeleton" className={classnames("animate-pulse rounded-md bg-slate-600", className)} {...props} />
+    <div data-slot="skeleton" className={classNames("animate-pulse rounded-md bg-slate-600", className)} {...props} />
   );
 }
 
