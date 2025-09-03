4b32ad6e295bcf761e978616edc98729c42c313e
Jono Mingard <reason.koan@gmail.com>
2025-09-03 12:20:29 +0100

    Fix

--- app/javascript/components/ui/Tabs.tsx
+++ app/javascript/components/ui/Tabs.tsx
@@ -1,7 +1,11 @@
 import cx from "classnames";
 import * as React from "react";
 
-export const Tabs = ({ children }: { children: React.ReactNode }) => <div className="flex gap-3">{children}</div>;
+export const Tabs = ({ children }: { children: React.ReactNode }) => (
+  <div role="tablist" className="flex gap-3">
+    {children}
+  </div>
+);
 
 export const Tab = ({
   children,
@@ -9,7 +13,8 @@ export const Tab = ({
   ...props
 }: { children: React.ReactNode; isSelected: boolean } & React.HTMLProps<HTMLAnchorElement>) => (
   <a
-    className={cx("shrink-0 rounded-full px-3 py-2 no-underline", isSelected && "border border-border bg-background")}
+    className={cx("shrink-0 rounded-full px-3 py-2 no-underline", isSelected && "border-border bg-background border")}
+    role="tab"
     aria-selected={isSelected}
     {...props}
   >
