5300a7fa82a1a882c3560ada6e9f7fbcfc76cbbd
Ershad Kunnakkadan <ershad92@gmail.com>
2025-09-05 14:30:27 -0400

    Add cache invalidation after stamped PDF email notification

--- app/services/pdf_stamping_service.rb
+++ app/services/pdf_stamping_service.rb
@@ -17,4 +17,9 @@ module PdfStampingService
   def stamp_for_purchase!(purchase)
     PdfStampingService::StampForPurchase.perform!(purchase)
   end
+
+  def cache_key_for_purchase(purchase_id)
+    "stamp_pdf_for_purchase_job_#{purchase_id}"
+  end
+
 end
