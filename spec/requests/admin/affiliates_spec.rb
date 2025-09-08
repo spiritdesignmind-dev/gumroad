e8d26d2ef943cd5d9843a0189a37b234b1770cb9
d1ddyup <srvk.k17@gmail.com>
2025-09-08 21:41:13 +0530

    added ajax

--- spec/requests/admin/affiliates_spec.rb
+++ spec/requests/admin/affiliates_spec.rb
@@ -3,17 +3,12 @@
 require "spec_helper"
 
 describe "Admin::AffiliatesController Scenario", type: :system, js: true do
+  include CapybaraHelpers
+
   let(:admin) { create(:admin_user, has_risk_privilege: true) }
   let(:affiliate_user) { create(:affiliate_user) }
 
   before do
-    # Mock the request_from_iffy? method to return false
-    allow_any_instance_of(Admin::BaseController).to receive(:request_from_iffy?).and_return(false)
-
-    # Stub the ObfuscateIds module constants since the view uses obfuscated IDs
-    stub_const("ObfuscateIds::CIPHER_KEY", "test_cipher_key_32_characters_long")
-    stub_const("ObfuscateIds::NUMERIC_CIPHER_KEY", 123456789)
-
     login_as(admin)
   end
 
@@ -25,8 +20,8 @@ describe "Admin::AffiliatesController Scenario", type: :system, js: true do
     it "shows no products alert" do
       visit admin_affiliate_path(affiliate_user)
 
-      # Click on the Products tab to make the content visible
       click_on "Products"
+      wait_for_ajax
 
       expect(page).to have_text("No affiliated products.")
     end
@@ -46,8 +41,8 @@ describe "Admin::AffiliatesController Scenario", type: :system, js: true do
     it "shows products" do
       visit admin_affiliate_path(affiliate_user)
 
-      # Click on the Products tab to make the content visible
       click_on "Products"
+      wait_for_ajax
 
       expect(page).to have_text("Product a")
       expect(page).to have_text("Product b")
